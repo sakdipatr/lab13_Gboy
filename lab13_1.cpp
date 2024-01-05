51a2ea64fe527270761167923c55f04eddf0daaa
gun <sadsaadw@gmail.com>
2024-01-05 13:50:42 +0700

    ad

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,23 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat (const double A[],int N,double B[]){
+    B[2] = 1.0;
+    for(int i = 0; i < N;i++){
+        B[0] += A[i];
+        B[2] *= A[i];
+        B[3] += 1/A[i]; 
+        if(A[i]>=B[4])B[4] = A[i];
+    }
+    B[0] = B[0]/N;
+    B[2] = pow(B[2], 1.0/N);
+    B[3] = N/B[3];
+    B[5] = A[0];
+    
+    for(int i = 0;i < N;i++){
+        B[1] += pow(A[i]-B[0],2);
+        if(A[i]<=B[5])B[5] = A[i];
+    }
+    B[1] = sqrt(B[1]/N);
+    
+}
